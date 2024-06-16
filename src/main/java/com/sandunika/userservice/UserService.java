cdf5a5959a378cdf66b4efc82e6b97a2ad3f0c0d
Sandunika <nayanahansi96@gmail.com>
2024-06-16 11:58:05 +0530

    Disable Redis cache

--- src/main/java/com/sandunika/userservice/UserService.java
+++ src/main/java/com/sandunika/userservice/UserService.java
@@ -1,9 +1,6 @@
 package com.sandunika.userservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
